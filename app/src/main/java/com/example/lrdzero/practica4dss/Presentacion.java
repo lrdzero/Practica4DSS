a79bcfaa285b7eca90740ae9c0ae337e7fc294de
lrdzero <elendil.capt.gondor@gmail.com>
2016-01-26 21:44:19 +0100

    cambios para adaptacion a tablet: "Unicamente corrección de visión"

--- app/src/main/java/com/example/lrdzero/practica4dss/Presentacion.java
+++ app/src/main/java/com/example/lrdzero/practica4dss/Presentacion.java
@@ -9,24 +9,33 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 
 public class Presentacion extends Activity implements View.OnClickListener {
     private Button btn1;
+    private WebView fg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presentacion);
         btn1 = (Button) findViewById(R.id.pagina);
-
+        fg=(WebView) findViewById(R.id.fragment);
+        fg.loadUrl("https://play.google.com/store/apps/category/GAME");
 
 
         btn1.setOnClickListener(this);
     }
 public void onClick(View v){
+    Intent myWebLink;
     switch (v.getId()){
         case R.id.pagina:
-            Intent myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
+            myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
+            myWebLink.setData(Uri.parse("https://play.google.com/store/apps/category/GAME"));
+            startActivity(myWebLink);
+            break;
+        case R.id.fragment:
+            myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
             myWebLink.setData(Uri.parse("https://play.google.com/store/apps/category/GAME"));
             startActivity(myWebLink);
             break;
